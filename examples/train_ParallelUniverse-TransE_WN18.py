b7c64efe00563b27af40e174eb3d1f27ea907813
Rashid Lafraie <rashidlafraie@hotmail.de>
2020-07-02 11:11:49 +0200

    Example file to train TransE in Parallel Universe configuration

---
+++ examples/train_ParallelUniverse-TransE_WN18.py
@@ -0,0 +1,68 @@
+from openke.config import Parallel_Universe_Config
+from openke.data import TrainDataLoader, TestDataLoader
+from openke.module.model import TransE, TransH
+import torch
+
+# noinspection PyPackageRequirements
+if __name__ == '__main__':
+    # Initialize random seed to make experiments reproducable
+    # init_random_seed = randint(0, 2147483647)
+    init_random_seed = 4
+
+    print("Initial random seed is:", init_random_seed)
+
+    # Initialize TrainDataLoader for sampling of examples
+    train_dataloader = TrainDataLoader(
+        in_path="../benchmarks/WN18/",
+        nbatches=100,
+        threads=8,
+        sampling_mode="normal",
+        bern_flag=1,
+        filter_flag=0,
+        neg_ent=1,
+        neg_rel=0,
+        initial_random_seed=init_random_seed)
+
+    # Initialize TestDataLoader which provides test data
+    # test_dataloader = TestDataLoader(train_dataloader.in_path, "link")
+    test_dataloader = TestDataLoader(train_dataloader.in_path, "classification")
+
+    # Set parameters for model used in the Parallel Universe Config (in this case TransE)
+    param_dict = {
+        'dim': 50,
+        'p_norm': 1,
+        'norm_flag': 1
+    }
+
+    embedding_method = TransE
+
+    PuTransE = Parallel_Universe_Config(
+        training_identifier='',
+        train_dataloader=train_dataloader,
+        test_dataloader=test_dataloader,
+        initial_num_universes=5000,
+        min_margin=1,
+        max_margin=4,
+        min_lr=0.01,
+        max_lr=0.1,
+        min_num_epochs=50,
+        max_num_epochs=200,
+        min_triple_constraint=500,
+        max_triple_constraint=2000,
+        min_balance=0.25,
+        max_balance=0.5,
+        embedding_model=embedding_method,
+        embedding_model_param=param_dict,
+        checkpoint_dir="./checkpoint/",
+        valid_steps=100,
+        save_steps=1000)
+
+
+    PuTransE.train_parallel_universes(10000)
+
+    # PuTransE.load_parameters("PuTransE_learned_spaces-5100.ckpt")
+    # state_dict = torch.load("./checkpoint/PuTransE_learned_spaces-540.ckpt", map_location='cpu')
+    # PuTransE.process_state_dict(state_dict)
+    # PuTransE.load_state_dict(state_dict)
+
+    PuTransE.run_link_prediction()
